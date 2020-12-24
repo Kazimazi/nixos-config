2287d3f0ebd7619c1aa57d2616babc5cac02b533
Attila Koszo <attila.koszo.official@gmail.com>
2020-12-24 17:13:20 +0100

    fix nvidia stuff

--- hosts/msi/configurations.nix
+++ hosts/msi/configurations.nix
@@ -3,7 +3,7 @@
 let
   nvidia-offload = pkgs.writeShellScriptBin "nvon" ''
     export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_PROVIDER=NVIDIA-G0
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
     export __GLX_VENDOR_LIBRARY_NAME=nvidia
     export __VK_LAYER_NV_optimus=NVIDIA_only
     exec -a "$0" "$@"
