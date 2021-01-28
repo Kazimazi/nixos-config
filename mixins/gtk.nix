43afba28c353f4b82718da68d0d6931afe866cc5
kazimazi <attila.koszo.official@gmail.com>
2021-01-28 02:20:49 +0100

    adwaita is default I think

--- mixins/gtk.nix
+++ mixins/gtk.nix
@@ -7,11 +7,11 @@
         enable = true;
         gtk3.extraConfig = {
           gtk-application-prefer-dark-theme=1;
-          gtk-cursor-theme-name="Adwaita";
+          #gtk-cursor-theme-name="Adwaita";
         };
       };
       home.packages = with pkgs; [
-        gnome3.adwaita-icon-theme
+        #gnome3.adwaita-icon-theme
         lxappearance
       ];
     };
