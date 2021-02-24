213037d3d4f57160e824875e54a6cbf545b03bea
kazimazi <attila.koszo.official@gmail.com>
2021-02-24 19:44:34 +0100

    don't care about vscode atm

--- mixins/vim/nvim/lua/init.lua
+++ mixins/vim/nvim/lua/init.lua
@@ -59,92 +59,90 @@ map('n', 'k', "(v:count == 0 ? 'gk' : 'k')", { noremap = true, expr = true, sile
 map('v', '<', '<gv', { noremap = true })
 map('v', '>', '>gv', { noremap = true })
 
-if fn.exists('g:vscode') == 0 then
-   local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
-   if fn.empty(fn.glob(install_path)) > 0 then
-      execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-      execute 'packadd packer.nvim'
-   end
-
-   -- Only required if you have packer in your `opt` pack
-   vim.cmd [[packadd packer.nvim]]
-
-   -- local use = require('packer').use -- not really needed but why is the linter complaining
-   return require('packer').startup(function()
-      -- Packer can manage itself as an optional plugin
-      use { 'wbthomason/packer.nvim', opt = true }
-      use { 'sheerun/vim-polyglot',}
-      use { 'tpope/vim-surround', event = 'VimEnter *' } -- Load on an autocommand event
-      use { 'andymass/vim-matchup', event = 'VimEnter *' }
-      use {'tpope/vim-fugitive'}
-      use { 'vim-airline/vim-airline' }
-
-      use 'preservim/nerdcommenter'
-      use {
-         'mhartington/oceanic-next',
-         config = function ()
-            vim.o.termguicolors = true
-            vim.g.oceanic_next_terminal_bold = 1
-            vim.g.oceanic_next_terminal_italic = 1
-            vim.cmd[[colorscheme OceanicNext]]
-         end
-      }
-
-
-      use { 'kyazdani42/nvim-web-devicons' }
-      -- use { 'glepnir/indent-guides.nvim' } -- indent-guides
-
-      -- use { 'glepnir/galaxyline.nvim' } -- looks very satisfactory
-      -- use { 'nvim-lua/lsp-status.nvim', } -- build it mayself?
-      -- use { 'romgrk/barbar.nvim', } -- looks cool
-
-      use { 'sbdchd/neoformat',
-            config = function()
-               vim.api.nvim_set_keymap('', '<leader>;', ':Neoformat<cr>', { noremap = true })
-               -- Have Neoformat use &formatprg as a formatter
-               vim.g.neoformat_try_formatprg = 1
-               -- Enable alignment
-               vim.g.neoformat_basic_format_align = 1
-               -- Enable tab to spaces conversion
-               vim.g.neoformat_basic_format_retab = 1
-               -- Enable trimmming of trailing whitespace
-               vim.g.neoformat_basic_format_trim = 1
-            end
-      }
-      use {
-         'lewis6991/gitsigns.nvim',
-         requires = { 'nvim-lua/plenary.nvim' },
-         config = function()
-            require('gitsigns').setup()
-         end
-      }
-      use { 'direnv/direnv.vim' }
-      use { 'junegunn/fzf' }
-      use {
-         'junegunn/fzf.vim',
-         config = function()
-            local map = vim.api.nvim_set_keymap
-            map('n', '<leader>e', ':Files<cr>', { noremap = true })
-            map('n', '<leader>r', ':Rg<cr>', { noremap = true })
-            map('n', '<leader>g', ':GFiles<cr>', { noremap = true })
-            map('n', '<leader>b', ':Buffers<cr>', { noremap = true })
-         end
-      }
-      use {
-         'nvim-telescope/telescope.nvim',
-         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
-      }
-      use {'sirver/UltiSnips'}
-      use {'honza/vim-snippets'}
-      use {'Olical/conjure'}
-      -- clojure stack
-      use {'clojure-vim/vim-jack-in',
-           requires = {{'tpope/vim-dispatch'}, {'radenling/vim-dispatch-neovim'}}}
-
-
-      -- require('built-in-lsp-combo')
-      require('coc')
-      require('treesitter')
-   end)
+if fn.empty(fn.glob(install_path)) > 0 then
+   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+   execute 'packadd packer.nvim'
 end
+
+-- Only required if you have packer in your `opt` pack
+vim.cmd [[packadd packer.nvim]]
+
+-- local use = require('packer').use -- not really needed but why is the linter complaining
+return require('packer').startup(function()
+   -- Packer can manage itself as an optional plugin
+   use { 'wbthomason/packer.nvim', opt = true }
+   use { 'sheerun/vim-polyglot',}
+   use { 'tpope/vim-surround', event = 'VimEnter *' } -- Load on an autocommand event
+   use { 'andymass/vim-matchup', event = 'VimEnter *' }
+   use {'tpope/vim-fugitive'}
+   use { 'vim-airline/vim-airline' }
+
+   use 'preservim/nerdcommenter'
+   use {
+      'mhartington/oceanic-next',
+      config = function ()
+         vim.o.termguicolors = true
+         vim.g.oceanic_next_terminal_bold = 1
+         vim.g.oceanic_next_terminal_italic = 1
+         vim.cmd[[colorscheme OceanicNext]]
+      end
+   }
+
+
+   use { 'kyazdani42/nvim-web-devicons' }
+   -- use { 'glepnir/indent-guides.nvim' } -- indent-guides
+
+   -- use { 'glepnir/galaxyline.nvim' } -- looks very satisfactory
+   -- use { 'nvim-lua/lsp-status.nvim', } -- build it mayself?
+   -- use { 'romgrk/barbar.nvim', } -- looks cool
+
+   use { 'sbdchd/neoformat',
+         config = function()
+            vim.api.nvim_set_keymap('', '<leader>;', ':Neoformat<cr>', { noremap = true })
+            -- Have Neoformat use &formatprg as a formatter
+            vim.g.neoformat_try_formatprg = 1
+            -- Enable alignment
+            vim.g.neoformat_basic_format_align = 1
+            -- Enable tab to spaces conversion
+            vim.g.neoformat_basic_format_retab = 1
+            -- Enable trimmming of trailing whitespace
+            vim.g.neoformat_basic_format_trim = 1
+         end
+   }
+   use {
+      'lewis6991/gitsigns.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = function()
+         require('gitsigns').setup()
+      end
+   }
+   use { 'direnv/direnv.vim' }
+   use { 'junegunn/fzf' }
+   use {
+      'junegunn/fzf.vim',
+      config = function()
+         local map = vim.api.nvim_set_keymap
+         map('n', '<leader>e', ':Files<cr>', { noremap = true })
+         map('n', '<leader>r', ':Rg<cr>', { noremap = true })
+         map('n', '<leader>g', ':GFiles<cr>', { noremap = true })
+         map('n', '<leader>b', ':Buffers<cr>', { noremap = true })
+      end
+   }
+   use {
+      'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+   }
+   use {'sirver/UltiSnips'}
+   use {'honza/vim-snippets'}
+   use {'Olical/conjure'}
+   -- clojure stack
+   use {'clojure-vim/vim-jack-in',
+        requires = {{'tpope/vim-dispatch'}, {'radenling/vim-dispatch-neovim'}}}
+
+
+   -- require('built-in-lsp-combo')
+   require('coc')
+   require('treesitter')
+end)
